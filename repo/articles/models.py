44832a17e428ba54ab4de90d06b07e852adacba5
martijnbentum <bentummartijn@gmail.com>
2021-08-18 14:41:15 +0200

    added articles class to store pdf info; add text class to store a specific pdf2text conversion of an pdf; added add_articles to add articles/texts to the database

---
+++ repo/articles/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+class ExtractionType(models.Model):
+	name = models.CharField(max_length=100,default = '', unique =True)
+
+	def __repr__(self):
+		return self.name
+
+
+class Text(models.Model):
+	dargs = {'on_delete':models.SET_NULL,'blank':True,'null':True}
+	filename = models.CharField(max_length=1000,default='')
+	extraction_type = models.ForeignKey(ExtractionType, **dargs, 
+		related_name = 'extraction_type')
+	text = models.TextField(default='')
+
+	def __repr__(self):
+		return 'Text: ' + self.name
+
+	@property
+	def nwords(self):
+		return len(text.replace('\n',' ').split(' '))
+
+	@property
+	def name(self):
+		return self.filename.split('/')[-1][:10] + '...\t' + self.text[:20] + '...'
+
+
+class Article(models.Model):
+	dargs = {'on_delete':models.SET_NULL,'blank':True,'null':True}
+	filename = models.CharField(max_length=1000,default='', unique = True)
+	ocr = models.ForeignKey(Text, **dargs, related_name = 'ocr')
+	pdftotext = models.ForeignKey(Text, **dargs, related_name = 'pdftotext')
+	pdfminer = models.ForeignKey(Text, **dargs, related_name = 'pdfminer')
+	info = models.TextField(default='')
+	error = models.BooleanField(default=False)
+	npages = models.IntegerField(default=0)
+	
+	def __repr__(self):
+		return 'Article: ' +self.name
+
+	@property
+	def nwords(self):
+		m = []
+		if self.ocr: m.append( 'ocr:'.ljust(12) + str(self.ocr.nwords))
+		if self.pdftotext: 
+			m.append( 'pdftotext:'.ljust(12) + str(self.pdftotext.nwords))
+		if self.pdfminer: 
+			m.append( 'pdfminer:'.ljust(12) + str(self.pdfminer.nwords))
+		return '\n'.join(m)
+
+	@property
+	def name(self):
+		return self.filename.split('/')[-1][:10] + '...'
+	
+	
+
+
+
+	
---
+++ repo/utils/add_articles.py
@@ -0,0 +1,63 @@
+import glob
+import sys
+import textract
+from articles.models import ExtractionType, Article, Text
+from PyPDF2 import PdfFileReader
+
+pdf_dir = '/vol/tensusers/mbentum/immuno/pdf/'
+
+def create_extraction_types():
+	for name in 'pdfminer,pdftotext,ocr'.split(','):
+		instance = ExtractionType(name = name)	
+		try:instance.save()
+		except:print('could not save extraction_type:',name)
+
+def get_all_pdf_filenames():
+	return glob.glob(pdf_dir + '*.pdf')
+
+
+def get_pypdf(filename):
+	fin = open(filename,'rb') 
+	return PdfFileReader(fin)
+
+def get_pdf_info(filename='', pdf = None):
+	if not pdf: pdf = get_pypdf(filename)
+	try: info = pdf.getDocumentInfo()
+	except:info = {'error':str(sys.exc_info())}
+	return info
+
+def extract_text_from_pdf(filename, method = 'pdfminer'):
+	if method == 'ocr':method='tesseract'
+	return textract.process(filename,method=method).decode()
+
+def make_text(filename,method = 'pdfminer', save = True):
+	et = ExtractionType.objects.get(name=method)
+	t = extract_text_from_pdf(filename, method)
+	instance = Text(filename=filename, text = t, extraction_type = et)
+	if save:
+		try: instance.save()
+		except:print('could not save text:',filename,instance,sys.exc_info())
+	return instance
+
+def make_article(filename, save = True):
+	pdf = get_pypdf(filename)
+	info = get_pdf_info(pdf=pdf)
+	npages = pdf.getNumPages()
+	ocr = make_text(filename,method='ocr')
+	pdftotext= make_text(filename,method='pdftotext')
+	pdfminer= make_text(filename,method='pdfminer')
+	error = 'error' in info.keys()
+	instance = Article(filename=filename,ocr=ocr,pdftotext=pdftotext,
+		pdfminer=pdfminer,info=info, error=error)
+	if save:
+		try: instance.save()
+		except:print('could not save article:',filename,instance,sys.exc_info())
+	return instance
+
+
+def add_articles(start = 0):
+	fn = get_all_pdf_filenames()
+	for i,f in enumerate(fn[start:]):
+		print(f,i,len(fn[start:]))
+		make_article(f)
+
